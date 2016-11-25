124918f85e186de3aff08bbb35ed99fcd09c4e19
Gordon Kristan <gordon.kristan@gmail.com>
2016-11-25 16:21:53 -0600

    Log purchase screen

--- src/components/pages/purchases/AddPurchase.js
+++ src/components/pages/purchases/AddPurchase.js
@@ -1,6 +1,59 @@
+import _ from 'lodash';
 import React from 'react';
 
 const AddPurchase = React.createClass({
+
+	getInitialState() {
+		return {
+			expenses: [],
+			cost: '',
+			category: undefined,
+			description: '',
+			date: ''
+		};
+	},
+
+	componentDidMount() {
+		const uid = firebase.auth().currentUser.uid;
+		this.dataRef = firebase.database().ref(`data/${uid}/budget/expenses`);
+		this.dataRef.on('value', this.expensesUpdated);
+	},
+
+	componentWillUnmount() {
+		this.dataRef.off('value', this.expensesUpdated);
+	},
+
+	////////////////////////////////////////
+
+	expensesUpdated(expensesSnapshot) {
+		this.setState({
+			expenses: expensesSnapshot.val()
+		});
+	},
+
+	updateValue(name, event) {
+		let value = event.target.value;
+		if (name === 'cost') {
+			value = parseInt(value, 10);
+		}
+
+		this.setState({
+			[name]: event.target.value
+		});
+	},
+
+	add() {
+		const stateProperties = ['cost', 'category', 'description', 'date'];
+		const purchase = _.pick(this.state, stateProperties);
+
+		const userId = firebase.auth().currentUser.uid;
+		firebase.database().ref(`data/${userId}/transactions/purchases`).push(purchase, () => {
+			this.setState(_.pick(this.getInitialState(), stateProperties));
+		});
+	},
+
+	////////////////////////////////////////
+
 	render() {
 		return (
 			<div>
@@ -14,30 +67,47 @@ const AddPurchase = React.createClass({
 							type='number'
 							pattern='[0-9]*'
 							inputMode='numeric'
+						    value={this.state.cost}
+						    onChange={this.updateValue.bind(null, 'cost')}
 						/>
 					</div>
 					<div className='form-group'>
 						<label htmlFor='expense-add-category'>Category</label>
-						<select id='expense-add-category' className='form-control'>
-							<option>Bars and Restaurants</option>
-							<option>Groceries</option>
-							<option>Gas</option>
-							<option>Bill</option>
-							<option>Household item</option>
-							<option>Other</option>
+						<select
+							id='expense-add-category'
+							className='form-control'
+						    value={this.state.category}
+						    onChange={this.updateValue.bind(null, 'category')}
+						>
+							{_.map(this.state.expenses, (expense, id) => {
+								return (
+									<option value={id} key={id}>
+										{expense.name}
+									</option>
+								);
+							})}
 						</select>
 					</div>
 					<div className='form-group'>
 						<label htmlFor='expense-add-description'>Description</label>
-						<input id='expense-add-description' className='form-control' />
+						<input
+							id='expense-add-description'
+							className='form-control'
+						    value={this.state.description}
+						    onChange={this.updateValue.bind(null, 'description')}
+						/>
 					</div>
 					<div className='form-group'>
 						<label htmlFor='expense-add-date'>Date</label>
-						<input id='expense-add-date' className='form-control' type='date'/>
+						<input
+							id='expense-add-date'
+							className='form-control'
+							type='date'
+						    value={this.state.date}
+						    onChange={this.updateValue.bind(null, 'date')}
+						/>
 					</div>
-					<button className='btn btn-success'>Add</button>
-					&nbsp;
-					<button className='btn btn-secondary'>Cancel</button>
+					<button className='btn btn-success' onClick={this.add}>Add</button>
 				</form>
 			</div>
 		);
