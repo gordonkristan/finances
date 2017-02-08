1f348727a6de055a129143b9a512ef5879a35dda
Gordon Kristan <gordon.kristan@gmail.com>
2017-02-07 21:11:10 -0600

    Convert purchase details to form

--- src/components/pages/purchases/PurchaseDetails.js
+++ src/components/pages/purchases/PurchaseDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Expense from 'app/models/expense';
 import Purchase from 'app/models/purchase';
+import Form from 'app/components/util/Form';
 
 class PurchaseDetails extends React.Component {
 	static propTypes = {
@@ -32,13 +33,13 @@ class PurchaseDetails extends React.Component {
 			cost: purchase.cost,
 			expenseId: purchase.expenseId,
 			description: purchase.description,
-			date: purchase.date.format('YYYY-MM-DD')
+			date: purchase.date
 		};
 	}
 
-	updateValue(key, event) {
+	onValueUpdated(key, value) {
 		this.setState({
-			[key]: event.target.value
+			[key]: value
 		});
 	}
 
@@ -49,70 +50,115 @@ class PurchaseDetails extends React.Component {
 		purchase.cost = parseInt(cost, 10);
 		purchase.description = description;
 		purchase.expenseId = expenseId;
-		purchase.date = date;
+		purchase.date = date.format('YYYY-MM-DD');
 	}
 
 	////////////////////////////////////////
 
 	render() {
+		const { expenses } = this.props.models;
+		const { cost, expenseId, description, date } = this.state;
+
+		const fields = [
+			{
+				type: 'number',
+				name: 'cost',
+				title: 'Cost',
+				value: cost
+			},
+			{
+				type: 'select',
+				name: 'expenseId',
+				title: 'Expense',
+				options: expenses.map(({ id, name }) => {
+					return { title: name, value: id };
+				}),
+				value: expenseId
+			},
+			{
+				type: 'string',
+				name: 'description',
+				title: 'Description',
+				value: description
+			},
+			{
+				type: 'date',
+				name: 'date',
+				title: 'Purchase Date',
+				value: date
+			}
+		];
+
 		return (
 			<div className='col-xs-12 col-md-8 offset-md-2'>
-				<h4>Purchase Details</h4>
-				<form>
-					<div className='form-group'>
-						<label htmlFor='purchase-edit-cost'>Cost</label>
-						<input
-							id='purchase-edit-cost'
-							className='form-control'
-							type='number'
-							pattern='[0-9]*'
-							inputMode='numeric'
-							value={this.state.cost}
-							onChange={this.updateValue.bind(this, 'cost')}
-						/>
-					</div>
-					<div className='form-group'>
-						<label htmlFor='purchase-edit-expense'>Expense</label>
-						<select
-							id='purchase-edit-expense'
-							className='form-control'
-							value={this.state.expenseId}
-							onChange={this.updateValue.bind(this, 'expenseId')}
-						>
-							{this.props.models.expenses.map((expense) => {
-								return (
-									<option value={expense.id} key={expense.id}>
-										{expense.name}
-									</option>
-								);
-							})}
-						</select>
-					</div>
-					<div className='form-group'>
-						<label htmlFor='purchase-edit-description'>Description</label>
-						<input
-							id='purchase-edit-description'
-							className='form-control'
-							value={this.state.description}
-							onChange={this.updateValue.bind(this, 'description')}
-						/>
-					</div>
-					<div className='form-group'>
-						<label htmlFor='purchase-edit-date'>Date</label>
-						<input
-							id='purchase-edit-date'
-							className='form-control'
-							type='date'
-							value={this.state.date}
-							onChange={this.updateValue.bind(this, 'date')}
-						/>
-					</div>
-					<button className='btn btn-success' onClick={this.save.bind(this)}>
-						Save
-					</button>
-				</form>
+				<Form
+					title='Purchase Details'
+					fields={fields}
+					submitText='Save'
+					onValueUpdated={this.onValueUpdated.bind(this)}
+					onSubmit={this.save.bind(this)}
+				/>
 			</div>
 		);
+
+		// return (
+		// 	<div className='col-xs-12 col-md-8 offset-md-2'>
+		// 		<h4>Purchase Details</h4>
+		// 		<form>
+		// 			<div className='form-group'>
+		// 				<label htmlFor='purchase-edit-cost'>Cost</label>
+		// 				<input
+		// 					id='purchase-edit-cost'
+		// 					className='form-control'
+		// 					type='number'
+		// 					pattern='[0-9]*'
+		// 					inputMode='numeric'
+		// 					value={this.state.cost}
+		// 					onChange={this.updateValue.bind(this, 'cost')}
+		// 				/>
+		// 			</div>
+		// 			<div className='form-group'>
+		// 				<label htmlFor='purchase-edit-expense'>Expense</label>
+		// 				<select
+		// 					id='purchase-edit-expense'
+		// 					className='form-control'
+		// 					value={this.state.expenseId}
+		// 					onChange={this.updateValue.bind(this, 'expenseId')}
+		// 				>
+		// 					{this.props.models.expenses.map((expense) => {
+		// 						return (
+		// 							<option value={expense.id} key={expense.id}>
+		// 								{expense.name}
+		// 							</option>
+		// 						);
+		// 					})}
+		// 				</select>
+		// 			</div>
+		// 			<div className='form-group'>
+		// 				<label htmlFor='purchase-edit-description'>Description</label>
+		// 				<input
+		// 					id='purchase-edit-description'
+		// 					className='form-control'
+		// 					value={this.state.description}
+		// 					onChange={this.updateValue.bind(this, 'description')}
+		// 				/>
+		// 			</div>
+		// 			<div className='form-group'>
+		// 				<label htmlFor='purchase-edit-date'>Date</label>
+		// 				<input
+		// 					id='purchase-edit-date'
+		// 					className='form-control'
+		// 					type='date'
+		// 					value={this.state.date}
+		// 					onChange={this.updateValue.bind(this, 'date')}
+		// 				/>
+		// 			</div>
+		// 			<button className='btn btn-success' onClick={this.save.bind(this)}>
+		// 				Save
+		// 			</button>
+		// 		</form>
+		// 	</div>
+		// );
 	}
 }
 
